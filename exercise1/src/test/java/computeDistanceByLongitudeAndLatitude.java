5b30e666853171e64a25deb4830751c6831fc309
George-Adrian POPA <gepopa@pentalog.com>
2019-05-21 22:07:30 +0300

    exercise 2 - compute the distance between two points on the surface of earth by longitude and laitude

---
+++ exercise1/src/test/java/computeDistanceByLongitudeAndLatitude.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+
+public class computeDistanceByLongitudeAndLatitude {
+
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Enter lan1:");
+        double lat1 = input.nextDouble();
+        System.out.println("Enter lon1:");
+        double lon1 = input.nextDouble();
+        System.out.println("Enter lat2:");
+        double lat2 = input.nextDouble();
+        System.out.println("Enter lon2:");
+        double lon2 = input.nextDouble();
+
+
+        final int R = 6371; // Radius of the earth
+
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                    + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                    * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = R * c * 1000; // convert to meters
+
+
+        distance = Math.pow(distance, 2);
+
+        System.out.println((Math.sqrt(distance)) + " Meters\n");
+
+
+    }
+}
+
+
