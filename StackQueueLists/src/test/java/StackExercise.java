915e0e588153d5bf86344bf2f75265b71b899172
George-Adrian POPA <gepopa@pentalog.com>
2019-05-24 23:25:00 +0300

    Stack exercise v1 - push letters one by one and pop them one by one

---
+++ StackQueueLists/src/test/java/StackExercise.java
@@ -0,0 +1,80 @@
+//tack problems :
+//
+//        Reverse the word    " Incomprehensibilities"      .
+//        Hint : You push the word to stack - letter by letter - and then pop letters from the stack.
+
+import java.util.Stack;
+
+public class StackExercise {
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+          //  System.out.println(stack.empty());
+
+        stack.push("I");
+        stack.push("n");
+        stack.push("c");
+        stack.push("o");
+        stack.push("m");
+        stack.push("p");
+        stack.push("r");
+        stack.push("e");
+        stack.push("h");
+        stack.push("e");
+        stack.push("n");
+        stack.push("s");
+        stack.push("i");
+        stack.push("b");
+        stack.push("i");
+        stack.push("l");
+        stack.push("i");
+        stack.push("t");
+        stack.push("i");
+        stack.push("e");
+        stack.push("s");
+        System.out.println(stack);
+        System.out.print(stack.peek());
+        stack.pop();
+        System.out.print(stack.peek());
+        stack.pop();
+        System.out.print(stack.peek());
+        stack.pop();
+        System.out.print(stack.peek());
+        stack.pop();
+        System.out.print(stack.peek());
+        stack.pop();
+        System.out.print(stack.peek());
+        stack.pop();
+        System.out.print(stack.peek());
+        stack.pop();
+        System.out.print(stack.peek());
+        stack.pop();
+        System.out.print(stack.peek());
+        stack.pop();
+        System.out.print(stack.peek());
+        stack.pop();
+        System.out.print(stack.peek());
+        stack.pop();
+        System.out.print(stack.peek());
+        stack.pop();
+        System.out.print(stack.peek());
+        stack.pop();
+        System.out.print(stack.peek());
+        stack.pop();
+        System.out.print(stack.peek());
+        stack.pop();
+        System.out.print(stack.peek());
+        stack.pop();
+        System.out.print(stack.peek());
+        stack.pop();
+        System.out.print(stack.peek());
+        stack.pop();
+        System.out.print(stack.peek());
+        stack.pop();
+        System.out.print(stack.peek());
+        stack.pop();
+        System.out.print(stack.peek());
+
+
+    }
+}
